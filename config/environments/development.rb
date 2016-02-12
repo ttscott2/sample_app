d2f26efec14daa9b9b912883e324d278d2f0e6e2
Tim Scott <timothytscott3@gmail.com>
2016-02-12 03:43:12 +0000

    Add account activations

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,9 +1,5 @@
 Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
 
-  # In the development environment your application's code is reloaded on
-  # every request. This slows down response time but is perfect for development
-  # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
   # Do not eager load code on boot.
@@ -12,10 +8,12 @@ Rails.application.configure do
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
-
+ 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :test
+  host = 'rails-tutorial-ttscott2.c9users.io'
+  config.action_mailer.default_url_options = { host: host }
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
